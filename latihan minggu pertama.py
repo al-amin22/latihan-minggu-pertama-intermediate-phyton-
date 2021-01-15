9f960aaf0a2c7c308f0fd1668e2232ed6683f760
al-aminctrl <m.al.amin2234@gmail.com>
2021-01-15 18:54:11 +0700

    latihan phyton intermediate yg ke 1

---
+++ latihan minggu pertama.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# latihan no1 membuat matrik 4x4 dengan nilai 0 hingga 3
+
+import numpy as np
+
+
+# In[6]:
+
+
+m = np.array([[0, 1, 2, 1], [2, 0, 0, 1], [1, 2, 3, 0], [3, 0, 2, 2]], dtype=float)
+a = 2
+m
+
+print (m+a)
+print (m*a)
+
+
+# In[7]:
+
+
+m.shape
+
+
+# In[8]:
+
+
+#
+
+
+# In[ ]:
+
+
+
+
